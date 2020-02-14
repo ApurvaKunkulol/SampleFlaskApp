269e0fe521adfd6a685a5019a2e4cc1a0816cc21
ApurvaKunkulol <kunkulol.apurva@gmail.com>
2020-02-14 19:55:29 +0530

    added templates exercise

--- project_start.py
+++ project_start.py
@@ -1,4 +1,4 @@
-from flask import Flask
+from flask import Flask, url_for, render_template
 from markupsafe import escape
 
 app = Flask(__name__)
@@ -6,7 +6,22 @@ app = Flask(__name__)
 
 @app.route("/")
 def index():
-    return "Hi! I am Flask, a Micro Web framework!!"
+    paragraph = """
+                    But I must explain to you how all this mistaken idea of denouncing pleasure and praising pain was 
+                    born and I will give you a complete account of the system, and expound the actual teachings of the 
+                    great explorer of the truth, the master-builder of human happiness. No one rejects, dislikes, or 
+                    avoids pleasure itself, because it is pleasure, but because those who do not know how to pursue 
+                    pleasure rationally encounter consequences that are extremely painful. Nor again is there anyone 
+                    who loves or pursues or desires to obtain pain of itself, because it is pain, but because 
+                    occasionally circumstances occur in which toil and pain can procure him some great pleasure. 
+                    To take a trivial example, which of us ever undertakes laborious physical exercise, except to 
+                    obtain some advantage from it? But who has any right to find fault with a man who chooses to enjoy 
+                    a pleasure that has no annoying consequences, or one who avoids a pain that produces no 
+                    resultant pleasure?
+    """
+    message = ""
+    template_variables = {"message": message, "paragraph": paragraph}
+    return render_template("hello.html", **template_variables)
 
 
 @app.route("/hello")
